98ca331b1083a0ae483852e5a1addb6ab89144ab
gianLuke <sanar@live.it>
2017-02-23 09:56:33 +0100

    Some not important

--- 1/NodeApp.java
+++ 1/NodeApp.java
@@ -70,7 +70,6 @@ public class NodeApp {
     }
 	
     public static void main(String[] args) {
-		
 		if (args.length != 0 && args.length !=2 ) {
 			System.out.println("Wrong number of arguments: [remote_ip remote_port]");
 			return;
