121f442320bd4e098ae5618f4947ccf74cd0c2f4
damiano <damiano@damiano-Aspire-V5-571G>
2017-03-28 09:40:50 +0200

    not existing item bug fixed

--- src/main/java/PendingRead.java
+++ src/main/java/PendingRead.java
@@ -15,11 +15,13 @@ public class PendingRead extends PendingOperation {
 	}
 
 	public void setLatestItem(Item item) {
-		if (this.item == null){
-			this.item = item;
-		} else {
-			if( this.item.getVersion() < item.getVersion()){
+		if (item!=null){
+			if (this.item == null){
 				this.item = item;
+			} else {
+				if( this.item.getVersion() < item.getVersion()){
+					this.item = item;
+				}
 			}
 		}
 		this.incrementCounter();
